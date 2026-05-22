3d1498bf5d28471f577511fcf9bf84b3b976b301
m4rche <marcelmuslija@pm.me>
2026-05-22 21:37:59 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+readonly NC=$(tput sgr0)
+readonly RED=$(tput setaf 1)
+readonly GREEN=$(tput setaf 2)
+readonly YELLOW=$(tput setaf 3)
+readonly BLUE=$(tput setaf 4)
+
+log_info()  { echo -e "[$(date '+%H:%M:%S')] ${GREEN}[INFO]${NC} $*"; }
+log_warn()  { echo -e "[$(date '+%H:%M:%S')] ${YELLOW}[WARN]${NC} $*"; }
+log_error() { echo -e "[$(date '+%H:%M:%S')] ${RED}[ERROR]${NC} $*"; }
+
+main() {
+  sudo -v 
+
+  set_display_resolution 1920 1080
+  set_keymap "hr"
+}
+
+set_display_resolution() {
+  local xorg_conf_d="/etc/X11/xorg.conf.d"
+
+  local x=$1
+  local y=$2
+  local resolution="${x}x${y}"
+
+  log_info "Setting display resolution ${resolution}"
+
+  local screen=$(xrandr | sed -n 2p | awk '{printf $1}')
+  log_info "Detected screen: ${screen}"
+
+  if ! xrandr --output "$screen" --mode "${resolution}"; then
+    log_warn "Failed to set resolution ${resolution} in active session"
+    return
+  fi
+
+  mkdir -p "${xorg_conf_d}"
+  cat > "${xorg_conf_d}/10-resolution.conf" << EOF
+  Section "Device"
+    Identifier  "Card0"
+    Driver      "modesetting"
+  EndSection
+  Section "Monitor"
+      Identifier  "${screen}"
+      Option      "PreferredMode" "${resolution}"
+  EndSection
+  Section "Screen"
+      Identifier  "Screen0"
+      Device      "Card0"
+      Monitor     "${screen}"
+      SubSection  "Display"
+          Depth   24
+          Modes   "${resolution}"
+      EndSubSection
+  EndSection
+  Section "ServerLayout"
+      Identifier  "Layout0"
+      Screen      "Screen0"
+  EndSection
+EOF
+
+  log_info "Resolution ${resolution} persisted in ${xorg_conf_d}/10-resolution.conf"
+}
+
+set_keymap() {
+  local kbd_conf="/etc/default/keyboard"
+
+  local keymap=$1
+
+  if ! keymap_exists "${keymap}"; then
+    log_warn "Keymap ${keymap} not found. Skipping step..."
+    return
+  fi
+
+  setxkbmap "${keymap}"
+  log_info "Set keymap ${keymap}"
+  
+  sudo sed -i "s/^XKBLAYOUT=.*/XKBLAYOUT=\"${keymap}\"/" "${kbd_conf}"
+  log_info "Keymap ${keymap} persisted in ${kbd_conf}"
+}
+
+keymap_exists() {
+  local keymap=$1
+
+  awk -v k="${keymap}" '$1 == k {found=1} END {exit !found}' /usr/share/X11/xkb/rules/xorg.lst
+}
+
+main "$@"
